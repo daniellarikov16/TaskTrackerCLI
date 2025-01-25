417d7e1ff3e683f17533c8197af7b7ce929b7eb0
daniellarikov16 <hopperlarikov@gmail.com>
2025-01-26 00:22:43 +0300

    commit

--- Tracker.py
+++ Tracker.py
@@ -1,11 +1,20 @@
 import json
+from datetime import datetime
 class Task:
-    def __init__(self, id = 0, description='',status='',createdAT='',updatedAT=''):
-        self.id = id
+    def __init__(self, description='',status='',createdAT='',updatedAT=''):
+        self.id = Task.get_id(self)
         self.description = description
         self.status = status
         self.createdAT = createdAT
         self.updatedAT = updatedAT
+    def get_id(self):
+        with open ('data.json', 'r') as file:
+            data = json.load(file)
+            items = data.get('tasks', [])
+        if not items:
+            return 1
+        max_id = max(item['id'] for item in items)
+        return max_id + 1
     def createTask(self):
         all_tasks = {'tasks': []}
         with open('data.json', 'r') as file:
@@ -13,9 +22,20 @@ class Task:
         all_tasks['tasks'].extend(data['tasks'])
         all_tasks['tasks'].append(self.__dict__)
         with open('data.json', 'w') as file:
-            json.dump(all_tasks, file, indent=4)  # Записываем весь словарь (все задачи) в файл
+            json.dump(all_tasks, file, indent=4)
         print("Данные успешно загружены в базу данных")
-new_task = Task()
-new_task.id = 2
-new_task.description = 'Полоть цветы'
-new_task.createTask()
+def get_all_task():
+    with open('data.json', 'r') as file:
+        data = json.load(file)
+        items = data.get('tasks', [])
+    return items
+now = datetime.now()
+ans = int(input())
+
+while ans != 0:
+    current_time = now.strftime("%H:%M:%S")
+    new_task = Task('Полоть цветы', 'Выполняется', current_time)
+    current_time = now.strftime("%H:%M:%S")
+    new_task.createTask()
+    ans = int(input())
+print(get_all_task())
